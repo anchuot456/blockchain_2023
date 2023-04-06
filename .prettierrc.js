249cedba0cc11ecff74301c3ff6fa3bb8850f1f0
Anh Vu Bui <buianhvu190999@gmail.com>
2023-04-06 14:19:26 +0200

    add prettier eslint

---
+++ .prettierrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+    printWidth: 120,
+    tabWidth: 2,
+    singleQuote: true,
+    jsxSingleQuote: true,
+    trailingComma: 'all',
+    bracketSpacing: true,
+    bracketSameLine: false,
+    arrowParen: 'always',
+    proseWrap: 'never',
+    htmlWhiteSpaceSensitivity: 'strict',
+    endOfLine: 'lf',
+}
